7940e7d0a281cc14b87c6dadadee5c84032b77f6
Orla Connelly <g00356556@gmit.ie>
2020-10-25 19:34:03 +0000

    lab complete

--- src/components/movieitem.js
+++ src/components/movieitem.js
@@ -1,20 +1,34 @@
 import React from 'react';
-// import 
+import Card from 'react-bootstrap/Card';
 
 
-export class Movieitem extends React.Component{
+export class Movieitem extends React.Component {
 
-    render(){
-        return(
+    render() {
+        return (
             <div>
-            
-            <h4> {this.props.movie.Title}</h4>
-            <p> {this.props.movie.Year}</p>
-            <img src= {this.props.movie.Poster} width="200" height="200"></img>
 
+
+
+
+
+                <Card>
+                    <Card.Header>{this.props.movie.Title}</Card.Header>
+                    <Card.Body>
+                        <blockquote className="blockquote mb-0">
+                            <img src={this.props.movie.Poster} width="200" height="200"></img>
+
+                            <footer className="blockquote-footer">
+                                <p> {this.props.movie.Year}</p>
+                            </footer>
+                        </blockquote>
+                    </Card.Body>
+                </Card>
             </div>
         );
     }
 
 }
-// added the create.js file to the app js 
+// added the create.js file to the app js
+
+// card from react bootstrap added to display movie info neater
