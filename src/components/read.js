c14db08f7effbc21055ede15c87be23ef23d0119
Orla Connelly <g00356556@gmit.ie>
2020-10-31 09:55:33 +0000

    form added

--- src/components/read.js
+++ src/components/read.js
@@ -1,41 +1,34 @@
 import React from 'react';
 import { Movies } from './movies';
+import axios from 'axios';
+
 // import 
 
 
-export class Read extends React.Component{
+export class Read extends React.Component {
 
-    state ={
-        movies: [
-            {
-            "Title": "Avengers: Infinity War",
-            "Year": "2018",
-            "imdbID": "tt4154756",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-            },
-            {
-            "Title": "Captain America: Civil War",
-            "Year": "2016",
-            "imdbID": "tt3498820",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-            },
-            {
-            "Title": "Charlie Wilson's War",
-            "Year": "2007",
-            "imdbID": "tt0472062",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMTgwMDgwMDc4MF5BMl5BanBnXkFtZTYwOTU3MDM4._V1_SX300.jpg"
+    state = {
+        movies: []
+
+    };
+
+    componentDidMount() {
+        axios.get('https://jsonblob.com/api/jsonblob/520c3b5e-0312-11eb-a6af-cbf00d776032')
+            .then((response) => {
+                this.setState({ movies: response.data.Search })
             }
-            ]
-            
+
+            )
+            .catch((error)=>{console.log(error)}
+            );
     }
-    render(){
-        return(
+
+
+    render() {
+        return (
             <div>
-            <h1>This is the Movies.</h1>
-            <Movies movies={this.state.movies}></Movies>
+                <h1>This is the Movies.</h1>
+                <Movies movies={this.state.movies}></Movies>
             </div>
         );
     }
@@ -43,7 +36,7 @@ export class Read extends React.Component{
 
 }
 
-    
+
 
 //added the read. js file to the app.js
 
@@ -51,3 +44,12 @@ export class Read extends React.Component{
 
 //movies component imported 
 //movies component i=embedded in read component
+
+
+//lab5 
+// imported axios package
+//Promise made
+//Arrow functions added 
+//data can be read from live api link isnetad of having to put all info in ourselves
+
+
